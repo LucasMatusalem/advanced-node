6f17fa9dfdcc3d31c5623540b7cf64d73b04ed96
LucasMatusalem <lucas@matusa.dev>
2024-01-09 21:09:36 -0300

    move FacebookAuthenticationService to its own file

--- jest.config.js
+++ jest.config.js
@@ -2,17 +2,20 @@
 const config = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   moduleNameMapper: {
     '@/tests/(.+)': '<rootDir>/tests/$1',
     '@/(.+)': '<rootDir>/src/$1'
   },
+  testMatch: ['**/*.spec.ts'],
   roots: [
     '<rootDir>/src',
     '<rootDir>/tests'
   ],
   transform: {
     '\\.ts$': 'ts-jest'
-  }
+  },
+  clearMocks: true
 }
 
 module.exports = config
